8db31e93be73fd976be897ec732b336389d06733
mohan-jayabalan <mohanstc@gmail.com>
2024-09-28 14:55:51 +0530

    SearchFish Commit

---
+++ Test Suites/TS_SearchFish.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_SearchFish</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f77f93d3-d119-4c4a-b0d2-24c4530960f7</testSuiteGuid>
+   <testCaseLink>
+      <guid>89cd36b1-fc52-4335-8d16-ea3f8067b875</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Search_Fish</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
