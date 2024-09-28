79bfae363779230ed6b07873c9952f9f2dfe1686
mohan-jayabalan <mohanstc@gmail.com>
2024-09-28 11:43:25 +0530

    Second Commit_ProductNavigation_Cats

---
+++ Test Suites/TS_ProductNavigation_Cats.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_ProductNavigation_Cats</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>df3315da-a78d-4f6e-b167-5504e3d09fb9</testSuiteGuid>
+   <testCaseLink>
+      <guid>200ecb74-9a26-4572-91c0-e0e2ed4e6f47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_ProductNavigation_Cats</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
