83dde31711718bb4b729cd38b054ad7ffc9d547c
mohan-jayabalan <mohanstc@gmail.com>
2024-10-04 13:30:12 +0530

    ProductNavigation_All

---
+++ Test Suites/TS_ProductNavigation_ALL.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_ProductNavigation_ALL</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>458fb080-032f-472d-a152-b75a32d96068</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ef1ac70-e3e4-4f13-bdb5-3d194f69a4bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Productnavigation_All</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
