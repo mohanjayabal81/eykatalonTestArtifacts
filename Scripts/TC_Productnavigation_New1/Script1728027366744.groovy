5e6d7b46bf18db32308c3de10faecc6e14596e61
mohan-jayabalan <mohanstc@gmail.com>
2024-10-04 13:27:47 +0530

    AI Commit

--- Scripts/TC_Productnavigation_New1/Script1728027366744.groovy
+++ Scripts/TC_Productnavigation_New1/Script1728027366744.groovy
@@ -42,3 +42,13 @@ WebUI.click(findTestObject('Object Repository/ProductNavigation_Fish/Page_JPetSt
 
 WebUI.closeBrowser()
 
+
+/* Write a Katalon Studio test case to perform the following steps.
+ * 1. Open browser to the URL stored in G_SiteURL
+ * 2. Click the make appointment button
+ * 3. Fill in the username and password fields based on the variables
+ * 4. Click the login button
+ * 5. Verify that the appointment div exists
+ * 6. Close the browser
+ * */
+
